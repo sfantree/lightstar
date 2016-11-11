cf561b5587ac196d0118b9648367075477e093e1
sfantree <panlingood@gmail.com>
2016-11-11 17:50:48 +0800

    first commite

---
+++ django_wsgi.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os
+import sys
+
+# 将系统的编码设置为UTF8
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_blog.settings")
+
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
